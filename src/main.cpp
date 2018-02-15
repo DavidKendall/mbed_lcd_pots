309e81ddf3735fa99dfb56e5f8f6065aedb12e57
David Kendall <david.kendall@northumbria.ac.uk>
2018-02-15 08:59:58 +0000

    Potentiometer solution

--- src/main.cpp
+++ src/main.cpp
@@ -3,18 +3,31 @@
 
 // Using Arduino pin notation
 C12832 lcd(D11, D13, D12, D7, D10);
+AnalogIn left(A0);
+AnalogIn right(A1);
+DigitalOut red(D5);
+DigitalOut green(D8);
+DigitalOut blue(D9);
 
 int main()
 {
-    int j=0;
+    red = 1;
+    green = 1;
+    blue = 1;
     lcd.cls();
-    lcd.locate(0,3);
-    lcd.printf("mbed application shield!");
+    lcd.locate(0,2);
+    lcd.printf("L:\n\n");
+    lcd.printf("R:");
+    lcd.rect(20, 0, 123, 9, 1);
+    lcd.rect(20, 18, 123, 27, 1);
 
     while(true) {   // this is the third thread
-        lcd.locate(0,15);
-        lcd.printf("Counting : %d",j);
-        j++;
-        wait(1.0);
+        int lboundary = 22 + (int)(100 * left.read());
+        int rboundary = 22 + (int)(100 * right.read());
+        lcd.fillrect(22, 2, lboundary, 7, 1);
+        lcd.fillrect(lboundary, 2, 121, 7, 0);
+        lcd.fillrect(22, 20, rboundary, 25, 1);
+        lcd.fillrect(rboundary, 20, 121, 25, 0);
+        wait_ms(100);
     }
 }
